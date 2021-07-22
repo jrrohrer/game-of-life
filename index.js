58244c3473bb17345b3579947d5ef5e832c9ccb6
jrrohrer <jessie.rohrer@gmail.com>
2021-07-22 11:04:33 -0400

    functions for finding neighbors and implementing game rules

--- index.js
+++ index.js
@@ -7,3 +7,65 @@ for (let y = 0; y < 100; y++) {
     field[y][x] = false;
   }
 }
+
+// Next, we need a function to get any cell's neighbors. A cell is identified by its X and Y values, so we can subtract and add 1 to those values to get all neighbors. Remember that the field is a donut shape, so we need to catch the border cases as well:
+
+const getNeighbors = (x, y, field) => {
+  let prevX = x - 1;
+  if (prevX < 0) {
+    prevX = field[0].length -1;
+  }
+
+  let nextX = x + 1;
+  if (nextX === field[0].length) {
+    nextX = 0;
+  }
+
+  let prevY = y - 1;
+  if (prevY < 0) {
+    prevY = field.length - 1;
+  }
+
+  let nextY = y + 1;
+  if (nextY === field.length) {
+    nextY = 0;
+  }
+
+  return [
+    field[prevY][prevX],
+    field[prevY][x],
+    field[prevY][nextX],
+    field[y][prevX],
+    field[y][nextX],
+    field[nextY][prevX],
+    field[nextY][x],
+    field[nextY][nextX]
+  ]
+}
+
+// This function returns an array of boolean values. The game's rules don't care about which neighbors are alive or dead, only how many of them are. The next step is to actally implement the rules. We need a function that takes x and y and returns the state of the cell for the next generation.
+
+const getDeadOrAlive = (x, y, field) => {
+  const neighbors = getNeighbors(x, y, field);
+  const numberOfAliveNeighbors = neighbors.filter(Boolean).length;
+
+  // Cell is alive
+
+  if (field[y][x]) {
+    if (numberOfAliveNeighbors < 2 || numberOfAliveNeighbors > 3) {
+      // cell dies
+      return false;
+    } 
+    // cell lives
+    return true;
+  }
+
+  // Cell is dead
+  if (numberOfAliveNeighbors === 3) {
+    // Cell lives
+    return true;
+  }
+  // Cell stays dead
+  return false;
+}
+
